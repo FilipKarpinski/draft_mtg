546af8e290feb0effd4241194808be709de675eb
fkarpinski <fkarpinski@gemius.com>
2025-03-07 22:08:12 +0100

    pre-commit fixes

--- app/db/database.py
+++ app/db/database.py
@@ -1,15 +1,20 @@
 from os import getenv
+from typing import Generator
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session, sessionmaker
 
 SQLALCHEMY_DATABASE_URL = getenv("DATABASE_URL")
+if SQLALCHEMY_DATABASE_URL is None:
+    raise ValueError("DATABASE_URL environment variable is not set")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-def get_db():
+
+def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
     try:
         yield db
--- app/db/models.py
+++ app/db/models.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Column, Integer, String
+
 from app.db.database import Base
+
 
 class Item(Base):
     __tablename__ = "items"
