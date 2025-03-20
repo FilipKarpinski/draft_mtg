19cb8bb4cd50b12601bbae831cfa81bd3f7a84fb
fkarpinski <fkarpinski@gemius.com>
2025-03-20 17:39:04 +0100

    ebe

--- app/config.py
+++ app/config.py
@@ -10,3 +10,8 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 PWD_CONTEXT = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+ORIGINS = [
+    "http://localhost",
+    "http://localhost:8080",
+]
--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 from app.auth.routers import login, users
+from app.config import ORIGINS
 from app.core.routers import drafts, matches, players
 
 app = FastAPI(title="Draft MTG API", description="API for managing MTG drafts", version="1.0.0")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(players.router)
